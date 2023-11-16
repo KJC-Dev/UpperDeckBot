d3e5ab56bdb470f9e632125a462ec6e7cccd5c7a
kjc-dev <mx@DESKTOP-OUTSET>
2023-11-15 23:26:24 -0700

    Added API functionalities
    Former-commit-id: 4bb4063553ee827de62175e9645406aa453b534b

--- README.md
+++
@@ -1,3 +0,0 @@
-# upperdeckBOT
-
-upperdeckBOT is a XMPP chatbot designed for interfacing with self-hosting
--- build-info.h
+++
@@ -1,7 +0,0 @@
-#ifndef BUILD_INFO_H
-#define BUILD_INFO_H
-
-#define BUILD_NUMBER 0
-#define BUILD_COMMIT "unknown"
-
-#endif // BUILD_INFO_H
